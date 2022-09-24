61739a126fbdcb59dcca3e00a99dc9af739a1bc0
Beatriz Souza Sartori <102486199+beatrizssartori@users.noreply.github.com>
2022-09-24 19:53:33 -0300

    Add files via upload

---
+++ quiz/src/data/questions_complete.js
@@ -0,0 +1,72 @@
+const data = [
+    {
+      category: "Diabetes Mellitus",
+      questions: [
+        {
+          question: "O que é a Diabetes Mellitus?",
+          options: [
+            "Excesso de açúcar/glicose no sangue", 
+            "Falta de açúcar/glicose no sangue", 
+            "Excesso de insulina no sangue", 
+            "Excesso de peso"],
+          answer: "Excesso de açúcar/glicose no sangue",
+          tip: "Também chamada de 'hiperglicemia'",
+        },
+        {
+          question: "Quais dos seguintes são sintomas da Diabetes?",
+          options: [
+            "Fome", 
+            "Fadiga", 
+            "Sede", 
+            "Urinar mais",
+            "Todas as respostas anteriores"],
+          answer: "Todas as respostas anteriores",
+        },
+        {
+          question: "O que sucede na Diabetes tipo 1?",
+          options: [
+            "O pâncreas não produz insulina", 
+            "O pâncreas produz insulina em excesso", 
+            "O pâncreas produz insulina ora em excesso, ora em níveis insuficientes"],
+          answer: "O pâncreas não produz insulina",
+        },
+        {
+          question: "O que sucede na Diabetes tipo 2?",
+          options: [
+            "O pâncreas deixa de produzir insulina", 
+            "O pâncreas produz níveis insuficientes de insulina", 
+            "O pâncreas produz níveis excessivos de insulina", 
+            "O pâncreas produz níveis insuficientes de insulina e/ou o organismo é resistente à insulina"],
+          answer: "O pâncreas produz níveis insuficientes de insulina e/ou o organismo é resistente à insulina",
+        },
+      ],
+    },
+    {
+      category: "Hipertensão Arterial",
+      questions: [
+        {
+          question: "Álcool em grande quantidade NÃO faz mal para quem tem hipertensão arterial.",
+          options: ["Verdadeiro", "Falso"],
+          answer: "Falso",
+        },
+        {
+          question: "A hipertensão, em muitos pacientes, não causa sintomas.",
+          options: ["Verdadeiro", "Falso"],
+          answer: "Verdadeiro",
+        },
+        {
+          question: "A pressão pode subir na frente do médico.",
+          options: ["Verdadeiro", "Falso"],
+          answer: "Verdadeiro",
+          tip: "Hipertensão do avental branco",
+        },
+        {
+          question: "As doenças cardiovasculares matam duas vezes mais que todos os tipos de câncer somados.",
+          options: ["Verdadeiro", "Falso"],
+          answer: "Verdadeiro",
+        },
+      ],
+    }
+  ];
+  
+export default data;
